1a6c5b4734740a7901c607c048ee0e8f5f85993f
邱伟豪#A04417 <weihao.qiu@casstime.com>
2022-09-03 12:58:30 +0800

    加载redis数据库、初始化JWT鉴权密钥、初始化路由、启动http监听端口

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"github.com/EDDYCJY/go-gin-demo/pkg/gredis"
 	"github.com/EDDYCJY/go-gin-demo/pkg/logging"
 	"github.com/EDDYCJY/go-gin-demo/pkg/models"
 	"github.com/EDDYCJY/go-gin-demo/pkg/setting"
+	"github.com/EDDYCJY/go-gin-demo/pkg/util"
+	"github.com/EDDYCJY/go-gin-demo/routers"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 // 初始化
@@ -15,9 +22,28 @@ func init() {
 	// 初始化日志打印 -> 捕获运行时的报错信息 —> 做好报错格式处理 -> 按等级划分
 	logging.Setup()
 	// redis 数据库
-	//gredis.Setup()
+	gredis.Setup()
+	// 初始化JWT鉴权密钥
+	util.Setup()
 }
 
 func main() {
-	//gin.SetMode()
+	// 运行时的环境 -> 选择debug模式
+	gin.SetMode(setting.ServerSetting.RunMode)
+	// 初始化路由 -> 所有的请求链路都将写入这里
+	routersInit := routers.InitRouter()
+	readTimeout := setting.ServerSetting.ReadTimeout
+	writeTimeout := setting.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%d", setting.ServerSetting.HttpPort)
+	maxHeaderBytes := 1 << 20
+
+	server := &http.Server{
+		Addr:           endPoint,
+		Handler:        routersInit,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+	}
+	log.Printf("[info] start http server listening %s", endPoint)
+	server.ListenAndServe()
 }
